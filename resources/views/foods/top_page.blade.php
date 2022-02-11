728e915d3bf83c8e411543f049edff17d154c7fc
chaki8923 <konkuriitonouenokareha128@gmail.com>
2022-02-12 07:36:11 +0900

    migrations-generatorインストール完了

--- resources/views/foods/top_page.blade.php
+++ resources/views/foods/top_page.blade.php
@@ -59,7 +59,6 @@
   </section>
   @endsection
   <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"></script>
-  <script src="/js/mycrop.js" defer></script>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
   <script src="https://cdn.tutorialjinni.com/pagePiling.js/1.5.6/jquery.pagepiling.min.js"></script>
