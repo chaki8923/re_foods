e3a9e1a48e96ea2cc1e1c1656bf7599586f7bbb2
chaki8923 <konkuriitonouenokareha128@gmail.com>
2022-02-23 12:21:28 +0900

    ファビコン設定

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -12,7 +12,7 @@
    
     <title>@yield('title') | {{ config('app.name') }}</title>
     <meta name="description" content="@yield('description')">
-
+    <link rel="shortcut icon" href="/images/apple.png" type="image/x-icon">
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
