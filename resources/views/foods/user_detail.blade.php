d6303ccdedb66d45d948cc1885398cdeb5e48fa5
chaki8923 <konkuriitonouenokareha128@gmail.com>
2022-02-25 06:25:58 +0900

    ユーザー詳細からの問い合わせリンク修正

--- resources/views/foods/user_detail.blade.php
+++ resources/views/foods/user_detail.blade.php
@@ -50,7 +50,7 @@
   <li class="footer-item"><a href="{{route('privacy')}}" class="footer-link">プライバシーポリシー</a></li>
   <li class="footer-item"><a href="{{route('rule')}}" class="footer-link">利用規約</a></li>
   <li class="footer-item"><a href="{{route('legal')}}" class="footer-link">特定商法取引法</a></li>
-  <li class="footer-item"><a href="" class="footer-link">お問い合わせ</a></li>
+  <li class="footer-item"><a href="{{route('contact')}}" class="footer-link">お問い合わせ</a></li>
 
 </ul>
 </footer>
